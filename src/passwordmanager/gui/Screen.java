62516b436b7e3fb827ac7fec480cd1c3270e48cd
hcps-jordannd <hcps-jordannd@180-18JORDANND.hcps.internal>
2016-05-06 10:26:59 -0400

    Works

---
+++ src/passwordmanager/gui/Screen.java
@@ -0,0 +1,39 @@
+package passwordmanager.gui;
+
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author hcps-jordannd
+ */
+public class Screen extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        Button btn = new Button();
+        btn.setText("Test Button");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Test Text");
+            }
+        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        
+        Scene scene = new Scene(root, 300, 250);
+        
+        primaryStage.setTitle("Test Screen");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
---
+++ src/passwordmanager/main/Main.java
@@ -0,0 +1,17 @@
+package passwordmanager.main;
+
+import javafx.application.Application;
+
+/**
+ *
+ * @author hcps-jordannd
+ */
+public class Main {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+}
